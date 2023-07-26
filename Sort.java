33e6f6356a6086cf4a562c11f013ba6cd7913cf1
Ankit Kumar <skwedsak143@gmail.com>
2023-07-26 11:23:58 +0530

    Selection sort in java

--- Sort.java
+++ Sort.java
@@ -13,6 +13,23 @@ public class Sort {
             }
         }
     }
+    // Selection Sort
+    public static void selectionSort(int arr[]){
+        for(int i=0;i<arr.length-1;i++)
+        {
+            int cmp=i;
+            for(int j=i+1;j<arr.length;j++)
+            {
+                if(arr[cmp]>arr[j])
+                cmp=j;
+            }
+            int temp = arr[cmp];
+            arr[cmp]=arr[i];
+            arr[i] = temp;
+        }
+
+    }
+
     public static void printArr(int arr[])
     {
         for(int i=0;i<arr.length;i++)
@@ -24,7 +41,9 @@ public class Sort {
     public static void main(String[] args) {
         int arr[]={5,4,3,2,1};
         printArr(arr);
-        bubbleSort(arr);
+        // bubbleSort(arr);
+        selectionSort(arr);
         printArr(arr);
+
     }
 }
